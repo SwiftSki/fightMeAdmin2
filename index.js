fb7fd141ed41b159a78ec3dce94ea4737dfa6c13
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-01-30 22:29:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert(word);
+var jq = document.createElement('script');
+jq.src='https://code.jquery.com/jquery-3.4.1.js';
+document.head.appendChild(jq);
+
+var guts = "";
+
+var blocker = document.createElement('div');
+blocker.style.top = '0px';
+blocker.style.left = '0px';
+blocker.style.position = 'fixed';
+blocker.style.width = '100%';
+blocker.style.height = '100%';
+blocker.style.background = 'blue';
+blocker.innerHTML = guts;
+document.body.appendChild(blocker);
