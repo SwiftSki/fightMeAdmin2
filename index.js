0a24d33f5f2691ded3e57a653a3ef2a32c59d84d
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-01-30 22:37:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,5 @@ var jq = document.createElement('script');
 jq.src='https://code.jquery.com/jquery-3.4.1.js';
 document.head.appendChild(jq);
 
-var guts = "";
-
-var blocker = document.createElement('div');
-blocker.style.top = '0px';
-blocker.style.left = '0px';
-blocker.style.position = 'fixed';
-blocker.style.width = '100%';
-blocker.style.height = '100%';
-blocker.style.background = 'blue';
-blocker.innerHTML = guts;
-document.body.appendChild(blocker);
+var guts = "furure login here";
+$("#secure").innerHTML = guts;
