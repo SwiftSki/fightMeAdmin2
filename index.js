a3917d3249d67f4d1b936c00c58536d1c9280707
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-01-30 22:45:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var jq = document.createElement('script');
 jq.src='https://code.jquery.com/jquery-3.4.1.js';
 document.head.appendChild(jq);
 
+var x = prompt('password','password');
+if(x==='password'){
+   $("#secure").hide();
+}
+/*
 $(document).ready(function(){
   var guts = "furure login here";
   //$("#secure").innerHTML = guts;
   document.getElementById("#secure").innerHTML = "hi";
 });
+*/
