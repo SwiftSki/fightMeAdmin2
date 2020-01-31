94cf5391ce086a9c05bbd3e1a248e5f206ea86e8
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-01-30 22:38:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ jq.src='https://code.jquery.com/jquery-3.4.1.js';
 document.head.appendChild(jq);
 
 var guts = "furure login here";
-$("#secure").innerHTML = guts;
+//$("#secure").innerHTML = guts;
+getElementById("#secure").innerHTML = "hi";
