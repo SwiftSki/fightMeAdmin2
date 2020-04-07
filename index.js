6599106e548742525b9eb4c5a4bc33bd0e3a5f3f
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-04-06 22:10:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function getCookie(cname) {
+    var name = cname + "="; //Create the cookie name variable with cookie name concatenate with = sign
+    var cArr = window.document.cookie.split(';'); //Create cookie array by split the cookie by ';'
+     
+    //Loop through the cookies and return the cooki value if it find the cookie name
+    for(var i=0; i<cArr.length; i++) {
+        var c = cArr[i].trim();
+        //If the name is the cookie string at position 0, we found the cookie and return the cookie value
+        if (c.indexOf(name) == 0) 
+            return c.substring(name.length, c.length);
+    }
+     
+    //If we get to this point, that means the cookie wasn't find in the look, we return an empty string.
+    return "";
+}
+
 var xp=document.createElement('div');
 xp.id='ohnoes';
 xp.innerHTML='oh noes';
@@ -10,20 +26,20 @@ xp.style.width='100%';
 xp.style.background='blue';
 xp.style.zIndex=999;
 document.body.appendChild(xp);
-/*
-var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)test\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+var test = getCookie("test");
 if(test==="sahara"){
     x = "sahara";
     $("#ohnoes").remove();
 }
-else{*/
+else{
     var str = prompt('password');
     var x = str.toLowerCase();
     if(x==='sahara'){
         $("#ohnoes").remove();
-        document.cookie="test=sahara";
+        window.document.cookie="test=sahara";
     }
-//}
+}
 /*
 $(document).ready(function(){
   var guts = "furure login here";
