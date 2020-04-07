9b6460fec1ff022e66b32808ffd996cf66fd7962
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-04-06 22:12:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ function getCookie(cname) {
     //If we get to this point, that means the cookie wasn't find in the look, we return an empty string.
     return "";
 }
+function deleteCookie(cname) {
+    var d = new Date(); //Create an date object
+    d.setTime(d.getTime() - (1000*60*60*24)); //Set the time to the past. 1000 milliseonds = 1 second
+    var expires = "expires=" + d.toGMTString(); //Compose the expirartion date
+    window.document.cookie = cname+"="+"; "+expires;//Set the cookie with name and the expiration date 
+}
 
 var xp=document.createElement('div');
 xp.id='ohnoes';
